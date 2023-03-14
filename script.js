b6341632fe1ccdc043d0717f9b4daecffb836ecb
Jai <jaijai.stell@gmail.com>
2023-03-14 00:42:20 -0700

    tried to create button will try again

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
 
+//<button onclick="document.getElementById('demo').innerHTML = Date()">The time is?</button>
