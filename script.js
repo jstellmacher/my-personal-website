3ceb35ed89f30510faa45c394e8a202f38dab599
Jai <jaijai.stell@gmail.com>
2023-03-14 02:18:02 -0700

    adds button with css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+//const h2 = document.createElement("h2");
+//h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+//document.querySelector("body").appendChild(h2);
 
 //<button onclick="document.getElementById('demo').innerHTML = Date()">The time is?</button>
