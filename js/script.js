5c40620d9545064065da47b71c0b10e590ecc638
Jai <jaijai.stell@gmail.com>
2023-03-18 19:42:57 -0700

    many css changes

--- js/script.js
+++ js/script.js
@@ -8,3 +8,21 @@ document.getElementById("tldr").addEventListener("click", tldrButton);
 function tldrButton() {
     document.getElementById("tldr").innerHTML = "Yay it Worked!";
 }
+
+
+
+//BUILDING A STICK JS NAVBAR!!!!!!!!!!!!!!!
+window.onscroll = function() {myFunction()};
+
+var menu = document.getElementsByTagName("menu");
+var sticky = menu.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    menu.classList.add("sticky")
+  } else {
+    menu.classList.remove("sticky");
+  }
+}
+//window.onscroll is similar to our document. stuff - it is considered an event
+//
